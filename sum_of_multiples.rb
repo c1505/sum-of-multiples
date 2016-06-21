f087b5777e0cb4aa80538bae46b406bd796a1969
c1505 <coreym247@gmail.com>
2016-06-21 16:01:43 +0000

    all tests passing

--- sum_of_multiples.rb
+++ sum_of_multiples.rb
@@ -1,7 +1,12 @@
+# take arguments
 class SumOfMultiples
+    def initialize(num1, num2, num3 = nil)
+        @num1 = num1
+        @num2 = num2
+        @num3 = num3
+    end
+    
     def self.to(num)
-        # multiples of 3 or 5 up to input number
-        # return only unique
         multiplier = 1
         result_array = []
         while (3 * multiplier) < num
@@ -19,9 +24,39 @@ class SumOfMultiples
             result = 0
         end
         result
-        # result_array.uniq
+    end
+    
+    def to(num)
+        multiplier = 1
+        result_array = []
+        while (@num1 * multiplier) < num
+            result_array << @num1 * multiplier
+            multiplier += 1
+        end
+        multiplier = 1
+        while (@num2 * multiplier) < num
+            result_array << @num2 * multiplier
+            multiplier += 1
+        end
+        multiplier = 1
+        if @num3
+            while (@num3 * multiplier) < num
+                result_array << @num3 * multiplier
+                multiplier += 1
+            end
+        end
+        
+        unless result_array.empty?
+            result = result_array.uniq.reduce(:+)
+        else
+            result = 0
+        end
+        result
     end
 end
+
+
+# 30, SumOfMultiples.new(4, 6).to(15)
 
 # If we list all the natural numbers up to but not including 20 that are
 # multiples of either 3 or 5, we get 3, 5, 6 and 9, 10, 12, 15, and 18.
