9a86c00481c2831551dcc04a761d80c61aedfdc4
c1505 <coreym247@gmail.com>
2016-07-19 00:38:10 +0000

    refactor.  extracted method.  renamed.  class method calls similar instance method

--- sum_of_multiples.rb
+++ sum_of_multiples.rb
@@ -1,67 +1,31 @@
-# take arguments
 class SumOfMultiples
-    def initialize(num1, num2, num3 = nil)
-        @num1 = num1
-        @num2 = num2
-        @num3 = num3
-    end
-    
-    def self.to(num)
-        multiplier = 1
-        result_array = []
-        while (3 * multiplier) < num
-            result_array << 3 * multiplier
-            multiplier += 1
-        end
-        multiplier = 1
-        while (5 * multiplier) < num
-            result_array << 5 * multiplier
-            multiplier += 1
-        end
-        unless result_array.empty?
-            result = result_array.uniq.reduce(:+)
-        else
-            result = 0
-        end
-        result
-    end
-    
-    def to(num)
-        multiplier = 1
-        result_array = []
-        while (@num1 * multiplier) < num
-            result_array << @num1 * multiplier
-            multiplier += 1
-        end
-        multiplier = 1
-        while (@num2 * multiplier) < num
-            result_array << @num2 * multiplier
-            multiplier += 1
-        end
-        multiplier = 1
-        if @num3
-            while (@num3 * multiplier) < num
-                result_array << @num3 * multiplier
-                multiplier += 1
-            end
-        end
-        
-        unless result_array.empty?
-            result = result_array.uniq.reduce(:+)
-        else
-            result = 0
-        end
-        result
-    end
+	attr_accessor :result_array
+	
+	def initialize(factor1, factor2, factor3 = nil)
+		@factor1 = factor1
+		@factor2 = factor2
+		@factor3 = factor3
+		@result_array = []
+	end
+
+	def self.to(limit)
+		inst = self.new(3, 5)
+		inst.to(limit)
+	end
+
+	def to(limit)
+		multiples(@factor1, limit)
+		multiples(@factor2, limit)
+		multiples(@factor3, limit) if @factor3
+		result = result_array.uniq.reduce(:+)
+		result ? result : 0
+	end
+
+	def multiples(factor, limit)
+		multiplier = 1
+		while (factor * multiplier) < limit
+			result_array << factor * multiplier
+			multiplier += 1
+		end
+	end
 end
-
-
-# 30, SumOfMultiples.new(4, 6).to(15)
-
-# If we list all the natural numbers up to but not including 20 that are
-# multiples of either 3 or 5, we get 3, 5, 6 and 9, 10, 12, 15, and 18.
-
-# The sum of these multiples is 78.
-
-# Write a program that can find the sum of the multiples of a given set of
-# numbers. If no set of numbers is given, default to 3 and 5.
